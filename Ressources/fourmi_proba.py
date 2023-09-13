97113e79d7c8d94d89c3697875074b689e1bda20
Clement FORNES <123367173+clementfornes13@users.noreply.github.com>
2023-09-13 17:21:48 +0200

    Update fourmi_proba.py

--- Ressources/fourmi_proba.py
+++ Ressources/fourmi_proba.py
@@ -1,8 +1,3 @@
-import random as rd
-import time
-import matplotlib.pyplot as plt
-import math
-import random
 import multiprocessing as mp
 import sys
 
@@ -13,33 +8,33 @@ if __name__ == "__main__":
     var1 = int(sys.argv[1])
     var2 = sys.argv[2]
     def distance_euclidienne(A,B):
-        #On calcule la distance euclidienne entre deux points
+        # On calcule la distance euclidienne entre deux points
         return ((A[0]-B[0])**2+(A[1]-B[1])**2)**(1/2)
 
     def fourmi_proba_process(i,resultat_queue,Graph,Pheromone,PositionFourmis,SeenFourmis,alpha=1,beta=1) :
-        #On filtre le cas ou la fourmi doit rejoindre son noeud d'origine
+        # On filtre le cas ou la fourmi doit rejoindre son noeud d'origine
         Last = True
         for bool in SeenFourmis[i] :
-            if bool == False :
+            if bool is False :
                 Last = False
         if Last :
-            #On renvoie le signal qu'une fourmi doit revenir à son point de départ
+            # On renvoie le signal qu'une fourmi doit revenir à son point de départ
             return ["F"]*len(PositionFourmis)
         Result_temp = []
         Distances_temp = []
-        #On la calculer pour chaque fourmis les probabilités d'aller à chaque noeud
-        #Coordonnées de la fourmis
+        # On la calculer pour chaque fourmis les probabilités d'aller à chaque noeud
+        # Coordonnées de la fourmis
         CoordsFourmis = Graph[PositionFourmis[i]]
         for j in range(len(Graph)) :
-            #j est le noeud de destination
-            #On veux pas que la fourmis reste sur place ni qu'elle aille sur un noeud qu'elle a déjà vu
+            # j est le noeud de destination
+            # On veux pas que la fourmis reste sur place ni qu'elle aille sur un noeud qu'elle a déjà vu
             if j != PositionFourmis[i] and SeenFourmis[i][j] == False:
                 Result_temp.append([PositionFourmis[i],j,(Pheromone[PositionFourmis[i]][j])**(beta)/(distance_euclidienne(CoordsFourmis,Graph[j]))**(alpha)])
                 Distances_temp.append((Pheromone[PositionFourmis[i]][j])**(beta)/(distance_euclidienne(CoordsFourmis,Graph[j]))**(alpha))
             else :
                 Result_temp.append((PositionFourmis[i], j, None))
                 Distances_temp.append(None)
-        #On return tout au thread principal
+        # On return tout au thread principal
         resultat_queue.put(Result_temp,Distances_temp)
 
     def fourmis_proba_main(Graph,Pheromone,PositionFourmis,SeenFourmis,alpha=1,beta=1) : #Trustable
@@ -53,20 +48,17 @@ if __name__ == "__main__":
             processes.append(process)
         for process in processes:
             process.join()
-        #On récupère les résultats
+        # On récupère les résultats
         for i in range(len(PositionFourmis)) :
             Result_temp,Distances_temp = resultat_queue.get()
             Result.append(Result_temp)
-            #On calcul la distance totale pour normaliser les probabilités (on prend pas en compte les None)
-            Distances.append(sum([element for element in Distances_temp if element != None]))
+            # On calcul la distance totale pour normaliser les probabilités (on prend pas en compte les None)
+            Distances.append(sum([element for element in Distances_temp if element is not None]))
 
-        #On normalise les probabilités
+        # On normalise les probabilités
         for i in range(len(Result)) :
             for j in range(len(Result[i])) :
-                if Result[i][j][2] != None :
-                    #On normalise les probabilités
+                if Result[i][j][2] is not None :
+                    # On normalise les probabilités
                     Result[i][j][2] = (Result[i][j][2]/Distances[i])
         return Result
-
-
-
